a5d6d4395790f28251010821e8ba2dac1f13ec06
ABDALHAMID <ABD.VOU@GMAIL.COM>
2024-01-01 15:33:52 +0100

    style the nos service

--- view/client/nosService.php
+++ view/client/nosService.php
@@ -1,78 +1,67 @@
 <!-- ======= Services Section ======= -->
 <section class="section-services section-t8">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="title-wrap d-flex justify-content-between">
-              <div class="title-box">
-                <h2 class="title-a">Nos Services</h2>
-              </div>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="title-wrap d-flex justify-content-between">
+          <div class="title-box">
+            <h2 class="title-a">Nos Services</h2>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card-box-c foo">
-              <div class="card-header-c d-flex">
-                <div class="card-box-ico">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card-box-c foo">
+          <div class="card-header-c d-flex">
+            <div class="card-title-c align-self-center">
+              <div class="glass-style-card">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="card-box-ico-style">
                   <span class="bi bi-cart"></span>
                 </div>
-                <div class="card-title-c align-self-center">
-                  <h2 class="title-c">Mode de vie</h2>
-                </div>
+                <div class="card-inner"><span>Mode de vie</span>
               </div>
-              <div class="card-body-c">
-                <p class="content-c">
-                  
-                </p>
-              </div>
-              <div class="card-footer-c">
-                
-                
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card-box-c foo">
-              <div class="card-header-c d-flex">
-                <div class="card-box-ico">
-                  <span class="bi bi-calendar4-week"></span>
-                </div>
-                <div class="card-title-c align-self-center">
-                  <h2 class="title-c">Prets</h2>
-                </div>
-              </div>
-              <div class="card-body-c">
-                <p class="content-c">
-                  
-                </p>
-              </div>
-              <div class="card-footer-c">
-                
-                
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card-box-c foo">
-              <div class="card-header-c d-flex">
-                <div class="card-box-ico">
-                  <span class="bi bi-card-checklist"></span>
-                </div>
-                <div class="card-title-c align-self-center">
-                  <h2 class="title-c">Vendre</h2>
-                </div>
-              </div>
-              <div class="card-body-c">
-                <p class="content-c">
-                  
-                </p>
-              </div>
-              <div class="card-footer-c">
-                
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section><!-- End Services Section -->
+      <div class="col-md-4">
+        <div class="card-box-c foo">
+          <div class="card-header-c d-flex">
+            <div class="card-title-c align-self-center">
+              <div class="glass-style-card">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="card-box-ico-style">
+                  <span class="bi bi-calendar4-week"></span>
+                </div>
+                <div class="card-inner"><span>Prets</span>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card-box-c foo">
+          <div class="card-header-c d-flex">
+            <div class="card-title-c align-self-center">
+              <div class="glass-style-card">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="card-box-ico-style">
+                  <span class="bi bi-card-checklist"></span>
+                </div>
+                <div class="card-inner"><span>Vendre</span>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section><!-- End Services Section -->
