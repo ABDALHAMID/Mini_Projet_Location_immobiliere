915eb3c8fe8c76802ac11d54e612af8c679fa873
ihssane-hiba <134102819+ihssane-hiba@users.noreply.github.com>
2024-01-06 20:24:58 +0100

    cc

--- view/admin/clientList.php
+++ view/admin/clientList.php
@@ -1,64 +1,65 @@
-
+<?php
+?>
 <div class="row">
 <div class="col-12 grid-margin">
   <div class="card">
     <div class="card-body">
-      <h4 class="card-title">Recent Tickets</h4>
+      <h4 class="card-title">Les clients récents</h4>
       <div class="table-responsive">
         <table class="table">
           <thead>
             <tr>
-              <th> Assignee </th>
-              <th> Subject </th>
+              <th> Nom </th>
+              <th> Prenom</th>
               <th> Status </th>
-              <th> Last Update </th>
-              <th> Tracking ID </th>
+              <th> Email </th>
+              
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>
-                <img src="assets/images/faces/face1.jpg" class="me-2" alt="image"> David Grey
+                <img src="assets/images/faces/face1.jpg" class="me-2" alt="image"> Bahan
               </td>
-              <td> Fund is not recieved </td>
+              <td> Kaoutar </td>
               <td>
-                <label class="badge badge-gradient-success">DONE</label>
+                <label class="badge badge-gradient-success">client</label>
               </td>
-              <td> Dec 5, 2017 </td>
-              <td> WD-12345 </td>
+              <td> kaoutar@gmail.com </td>
+              
             </tr>
             <tr>
               <td>
-                <img src="assets/images/faces/face2.jpg" class="me-2" alt="image"> Stella Johnson
+                <img src="assets/images/faces/face2.jpg" class="me-2" alt="image"> Bouihi
               </td>
-              <td> High loading time </td>
+              <td> Taha </td>
               <td>
-                <label class="badge badge-gradient-warning">PROGRESS</label>
+                <label class="badge badge-gradient-warning">client</label>
               </td>
-              <td> Dec 12, 2017 </td>
-              <td> WD-12346 </td>
+              <td> taha@gmail.com </td>
+              <
             </tr>
             <tr>
               <td>
-                <img src="assets/images/faces/face3.jpg" class="me-2" alt="image"> Marina Michel
+                <img src="assets/images/faces/face3.jpg" class="me-2" alt="image"> Aouli
               </td>
-              <td> Website down for one week </td>
+              <td> Abdeljalil</td>
               <td>
-                <label class="badge badge-gradient-info">ON HOLD</label>
+                <label class="badge badge-gradient-info">client</label>
               </td>
-              <td> Dec 16, 2017 </td>
-              <td> WD-12347 </td>
+              <td> abdeljalil@gmail.com </td>
+              
             </tr>
             <tr>
               <td>
-                <img src="assets/images/faces/face4.jpg" class="me-2" alt="image"> John Doe
+                <img src="assets/images/faces/face4.jpg" class="me-2" alt="image">Mojahid
               </td>
-              <td> Loosing control on server </td>
+              <td> Hamza </td>
               <td>
-                <label class="badge badge-gradient-danger">REJECTED</label>
+                <label class="badge badge-gradient-danger">client</label>
               </td>
-              <td> Dec 3, 2017 </td>
-              <td> WD-12348 </td>
+              <td> hamza@gmail.com </td>
+              
             </tr>
           </tbody>
         </table>
