0bd2defa07e0b23de836767ba69d1d262c155f7b
ihssane-hiba <134102819+ihssane-hiba@users.noreply.github.com>
2024-01-06 18:48:08 +0100

    icon

--- index.php
+++ index.php
@@ -8,9 +8,10 @@ require('database_connection.php');
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>location immobiliere</title>
+  <title>rentit</title>
   <!-- <link href="assets/img/favicon.png" rel="icon"> -->
-  <link href="assets/img/icons/retiticon.png" rel="icon">
+  <link href="assets/img/icons/retiticon.ico" rel="icon">
+
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
