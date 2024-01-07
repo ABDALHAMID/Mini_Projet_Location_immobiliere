a4428336c4df4c9c582f9cf313c69e8ffbb7b829
ABDALHAMID <ABD.VOU@GMAIL.COM>
2024-01-07 14:52:50 +0100

    fix chat css

--- view/client/Chat.php
+++ view/client/Chat.php
@@ -1,11 +1,18 @@
+
 <div class="chat d-none" id='dialogue'>
-    <div class="chat-header">Rent it Chat</div>
-      <div class="chat-window">
-        <ul class="message-list"></ul>
-      </div>
-      <div class="chat-input">
-          <input type="text" class="message-input" placeholder="Entrer le message">
-          <button class="send-button">↑</button>
-      </div>
+  <div class="chat-header">Rent it Chat
+    <div id="dialoguef" class="d-none">
+      <button class="cat-close-button" onclick="closeDialogue()">
+      <i class="bi bi-x-lg"></i>
+        <div class="close-div">Close</div>
     </div>
-  
+    </button>
+  </div>
+  <div class="chat-window">
+    <ul class="message-list"></ul>
+  </div>
+  <div class="chat-input">
+    <input type="text" class="message-input" placeholder="Entrer le message">
+    <button class="send-button" id="send-button">↑</button>
+  </div>
+</div>
